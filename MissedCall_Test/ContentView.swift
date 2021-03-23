46f52fdce4476e293a2feae2e9fa56827452a9d1
Dmitrii Proshutinskii <koptehe@mail.ru>
2021-03-24 00:29:27 +0300

    Initial Commit

---
+++ MissedCall_Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MissedCall_Test
+//
+//  Created by Физтех.Радио on 24.03.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
